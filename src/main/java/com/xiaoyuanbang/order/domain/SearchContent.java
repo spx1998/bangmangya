ab6a2f60bda0f401423934401bf43e9e69f1da7e
kylesun <457799096@qq.com>
2019-05-15 23:04:26 +0800

    add searchContent

---
+++ src/main/java/com/xiaoyuanbang/order/domain/SearchContent.java
@@ -0,0 +1,13 @@
+package com.xiaoyuanbang.order.domain;
+
+public class SearchContent {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
