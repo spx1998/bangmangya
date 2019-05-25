6cf5fc7921351107b8a62c209e3b829440156707
kylesun <457799096@qq.com>
2019-05-25 10:42:38 +0800

    new ssl

--- src/main/java/com/xiaoyuanbang/common/domain/APPINFO.java
+++ src/main/java/com/xiaoyuanbang/common/domain/APPINFO.java
@@ -1,7 +1,7 @@
 package com.xiaoyuanbang.common.domain;
 
 public class APPINFO {
-    public final static String APP_ID="wx18ef9653f1586853";
-    public final static String APP_SECRET="4f3894559fb77f7e6fb33387abac05a2";
+    public final static String APP_ID="wx0dcecd2807cc958e";
+    public final static String APP_SECRET="7611a367bfaa0fce0b27162dd68224bd";
     public final static String GRANT_TYPE="authorization_code";
 }
